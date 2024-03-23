129fe1048c9ee37b282d94966583d4b44b49efe1
RedBlizard <grote2207@gmail.com>
2024-03-23 11:26:09 +0100

    small logfile fix install.sh

--- install.sh
+++ install.sh
@@ -6,20 +6,20 @@
 echo "Install script version release 1.07"
 echo "Developed by RedBlizard date 10 December 2023"
 
-
-# ---------------------
 # Set the log file path
-# ---------------------
 log_file="$HOME/installation_log.txt"
 
-# --------------------------------------------------
+# Function to log messages to the file
+log_message() {
+    echo "$(date +"%Y-%m-%d %H:%M:%S") - $1" >> "$log_file"
+}
+
 # Redirect stdout (1) and stderr (2) to the log file
-# --------------------------------------------------
-exec > >(tee -i "$log_file") 2>&1
+exec > "$log_file" 2>&1
 
 # Ensure the script is in the correct directory
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-cd "$SCRIPT_DIR" || { echo 'Failed to change directory to script directory.'; exit 1; }
+cd "$SCRIPT_DIR" || { log_message 'Failed to change directory to script directory.';
 
 # ------------------------------------------------------
 # Check if yay is installed
