f3bc90a324512a5da5385561c12a18465d3141e8
RedBlizard <108489214+RedBlizard@users.noreply.github.com>
2024-04-13 20:12:42 +0200

    Update install.sh
    fix for arch packages

--- install.sh
+++ install.sh
@@ -667,9 +667,6 @@ install_packages() {
     $helper -S --noconfirm $package_list
 }
 
-# Define colors
-RED='\033[1;31m'
-NC='\033[0m' # No Color
 
 # Extract Arch package list from packages-repository.txt
 arch_packages=$(awk '/^# AUR/ {exit} NF {print $0}' packages-repository.txt)
@@ -677,7 +674,24 @@ arch_packages=$(awk '/^# AUR/ {exit} NF {print $0}' packages-repository.txt)
 # Install Arch packages listed in packages-repository.txt
 if [ -n "$arch_packages" ]; then
     sudo pacman -Sy --noconfirm $arch_packages
-    echo -e "${RED}Arch packages successfully installed.${NC}"
+    missing_packages=""
+    for package in $arch_packages; do
+        if ! pacman -Qq "$package" &>/dev/null; then
+            missing_packages="$missing_packages $package"
+        fi
+    done
+    if [ -z "$missing_packages" ]; then
+        echo -e "${RED}Arch packages successfully installed.${NC}"
+    else
+        echo -e "${RED}Failed to install the following Arch packages:${NC} $missing_packages"
+#       echo -e "${RED}Attempting to install missing packages with yay...${NC}"
+        yay -Sy --noconfirm $missing_packages
+        if [ $? -eq 0 ]; then
+            echo -e "${RED}Missing Arch packages successfully installed.${NC}"
+        else
+            echo -e "${RED}Failed to install missing Arch packages.${NC}"
+        fi
+    fi
 else
     echo "No Arch packages found."
 fi
@@ -686,11 +700,14 @@ fi
 aur_packages=$(awk '/^# AUR/ {p=1; next} /^#/ {p=0} p' packages-repository.txt)
 if [ -n "$aur_packages" ]; then
     install_packages "$aur_helper" "$aur_packages"
-    echo -e "${RED}AUR packages successfully installed.${NC}"
+    if yay -Qq $aur_packages &> /dev/null; then
+        echo -e "${RED}AUR packages successfully installed.${NC}"
+    else
+        echo -e "${RED}Failed to install AUR packages.${NC}"
+    fi
 else
     echo "No AUR packages found."
 fi
-
 
 YELLOW='\033[1;33m'
 GREEN='\033[0;32m'
